0dd3570f4db2b2fa47907698a77a9b83c169c886
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-09-12 20:07:16 -0300

    commit 8

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,29 +1,62 @@
-import React from 'react'
-import { Container, Row, Col, Button } from 'react-bootstrap'
-import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { Container, Row, Col, Button, Table } from 'react-bootstrap'
+import { useNavigate } from "react-router-dom";
+import Appbar from '../components/AppBar';
+import { listaProdutos, ListaProdutoTypes } from '../utils/lista_produtos';
 
 export default function Homepage() {
   const navigate = useNavigate();
 
+  const [data, setData] = useState<ListaProdutoTypes[]>([]);
+
+  useEffect(() => {
+    setData(listaProdutos);
+  }, [])
+
+
   return (
-    <Container
-      className="d-flex justify-content-center pt-5"
-      fluid
-    >
-      <Row>
-        <Col xs={12}>
-          <h1 className="text-center">Homepage</h1>
-        </Col>
-        <Col xs={12} className="d-flex justify-content-center mt-5">
-          <Link to="/" className="btn btn-primary">Voltar</Link>
-        </Col>
-        <Col xs={12} className="d-flex justify-content-center mt-2">
-          <Button
-            variant="primary"
-            onClick={() => navigate("/")}
-          >Voltar</Button>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Appbar />
+      <Container
+        className="d-flex justify-content-center pt-5"
+        fluid
+      >
+        <Row>
+          <Col xs={12}>
+            <h1 className="text-center">Homepage</h1>
+          </Col>
+          <Col xs={12} className="d-flex justify-content-center mt-5">
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>Preço</th>
+                  <th className="text-end">#</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item.nome}</td>
+                    <td>{`R$ ${(item.preco).toFixed(2).replace(".", ",")}`}</td>
+                    <td align="right">
+                      <Button
+                        variant="primary"
+                      >Comprar</Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Col>
+          <Col xs={12} className="d-flex justify-content-center mt-2">
+            <Button
+              variant="primary"
+              onClick={() => navigate("/")}
+            >Sair</Button>
+          </Col>
+        </Row>
+      </Container>
+    </>
   )
 }
